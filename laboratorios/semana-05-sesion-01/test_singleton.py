f28316572aca259d4cf9dc5864906f6f83df799f
JhoanL2005 <jhoanlaica2005@outlook.com>
2026-09-15 19:03:25 +0000

    Clase Semana 5

--- laboratorios/semana-05-sesion-01/test_singleton.py
+++ laboratorios/semana-05-sesion-01/test_singleton.py
@@ -8,6 +8,10 @@ def test_rechaza_reserva():
 
 def test_acepta_reserva():
     config = GestorDeConfiguracion.obtener_objeto()
-
+    config.modo_mantenimiento = False
+    #El problema va cuando el modo de mantenimiento se mantiene con lo de la anterior prueba por lo que cambiamos al modo de mantenimiento a su estado original para hacer pasar la prueba
     assert reserva_permitida(config) is True
+
+
+
 
